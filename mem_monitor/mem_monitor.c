35c9db39317761a242acdeef33791aec3cdc29cb
Navjot Singh <navisinghnz9@gmail.com>
2019-07-23 11:46:02 +1200

    preventing preprocessor on malloc from infinite recursion

--- mem_monitor/mem_monitor.c
+++ mem_monitor/mem_monitor.c
@@ -4,17 +4,19 @@
 #include "mem_monitor.h"
 
 
+/*
+ * preventing preprocessor on malloc call in this file to call malloc_monitor
+ * leading to an infinite recursion
+ */
+#define malloc malloc
+
+
 void* malloc_monitor(size_t size, const char *file, int line, const char *func) {
 
-    printf ("malloc_monitor \n");
-
-    int x = 0;
-
     void *ptr = malloc(size);
-
     printf ("Allocated = %s, %i, %s, %p[%li]\n", file, line, func, ptr, size);
 
-    // ToDo - Add the mem allocation info to linked list
+    // FIXME - Add the mem allocation info to linked list
 
     return ptr;
 }
